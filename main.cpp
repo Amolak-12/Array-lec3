5563172a21a8f5147a470aa5fba1cf41b021ec10
Amolak-12 <133849186+Amolak-12@users.noreply.github.com>
2024-05-16 12:52:17 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+using namespace std;
+
+int main() {
+
+  // Problem Solving
+
+  //
+  // 1. Row-Sum Print
+  //
+
+
+  // int arr[4][4] = { {1,2,3,4} , {2,3,4,1} , {5,6,9,7} , {11,13,15,17} };
+
+  // for(int i = 0 ;i < 4;i++)
+  //   {
+  //     int sum = 0;
+  //     for(int j = 0;j < 4 ; j++)
+  //       {
+  //         sum = sum+arr[i][j];
+
+  //       }
+  //      cout << "Sum of " << i << "th row = " << sum << endl;
+  //   }
+
+  //
+  // 2. Max/Min in 2-D Array
+  //
+
+
+  // int brr[3][3] = { {1,2,3} , {4,5,6} , {7,8,9} };
+
+  // int maxi = INT_MIN;
+  // int mini = INT_MAX;
+
+  // for(int i = 0;i < 3;i++)
+  //   {
+  //     for(int j = 0;j < 3;j++)
+  //       {
+  //         if(brr[i][j] > maxi)
+  //         {
+  //           maxi = brr[i][j];
+  //         }
+  //       }
+  //   }
+
+  // for(int i = 0;i < 3;i++)
+  //   {
+  //     for(int j = 0;j < 3;j++)
+  //       {
+  //         if(brr[i][j] < mini)
+  //         {
+  //           mini = brr[i][j];
+  //         }
+  //       }
+  //   }
+  // cout << " The maximum no. is : " << maxi << endl;
+  // cout << " The minimum no. is : " << mini << endl;
+
+
+  // // 3. Transpose of a Matrix
+
+  // int crr[3][3] = { {1,2,3} , {4,5,6} , {7,8,9} };
+
+  // int drr[3][3];
+  // for(int i = 0;i < 3;i++)
+  // {
+  //   for(int j = 0;j < 3;j++)
+  //     {
+  //       cout << crr[i][j] << " ";
+  //     }
+  //   cout << endl;
+  // }
+
+  // for(int i = 0;i < 3;i++)
+  //   {
+  //     for(int j = 0;j < 3;j++)
+  //       {
+  //         swap(crr[i][j] , drr[j][i]);
+  //       }
+  //   }
+
+  // for(int i = 0;i < 3;i++)
+  // {
+  //   for(int j = 0;j < 3;j++)
+  //     {
+  //       cout << drr[i][j] << " ";
+  //     }
+  //   cout << endl;
+  // }
+
+
+  // Initializing 2-D vector with 1 no.
+  // vector<vector<int> > arr(5,vector<int>(5,65));
+
+  // for(int i = 0;i < arr.size();i++)
+  //   {
+  //     for(int j = 0;j < arr[i].size();j++)
+  //       {
+  //         cout << arr[i][j] << " ";
+  //       }
+  //     cout << endl;
+  //   }
+}
